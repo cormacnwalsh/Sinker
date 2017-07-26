b94d792af16567a815d04f4788b9b21ecdb90ded
cormacnwalsh <cormacnwalsh@gmail.com>
2017-07-26 16:13:28 +0100

    Add files via upload

--- src/sinker/Sinker.java
+++ src/sinker/Sinker.java
@@ -18,34 +18,45 @@ public class Sinker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Welcome to GridShip by Old Man Lally");
+        JOptionPane.showMessageDialog(null,"Welcome to GridShip");//Start Screen
         
-        Grid g = new Grid();
+        Grid g = new Grid();//Creates hidden Grid
         
-        g.fillGrid();
+        g.fillGrid();//Fills the grid with water
         
-        Ship s = new Ship();
+        Ship s = new Ship();//Generates Ship
         
-        g.grid[(int)(Math.random()*6)] = (s.ship1);
+        g.grid[(int)(Math.random()*6)] = (s.ship1);//Places Ship in random space
         
-        char[] play = new char[6];
+        char[] play = new char[6];//Creates visible grid
+        
+        for(int i = 0; i<play.length; i++){
+            play [i] = '-';//Fills visible grid with blank spaces
+        }
 
-        for(int i = 0; i < g.grid.length; i++){
+        for(int i = 0; i < g.grid.length; i++){//Game Loop
             
-            int shot = Integer.parseInt(JOptionPane.showInputDialog(Arrays.toString(g.grid) + "\n"
+            int shot = Integer.parseInt(JOptionPane.showInputDialog(Arrays.toString(play) + "\n"//selects target space
             + "Select Target"))-1;
             
-            if(g.grid[shot]=='V'){
-                JOptionPane.showMessageDialog(null,"You sunk my gridShip TM.\n"
-                        + "BINGO");
-                break;
+            if(play[shot]=='-'){//checks oof target space has not been selected before
                 
-            }else if(g.grid[shot]=='-'){
-                g.grid[shot]='X';
+                if(g.grid[shot]=='V'){//if target contains a ship
+                    JOptionPane.showMessageDialog(null,"You sunk my gridShip TM.\n"
+                            + "BINGO");
+                    break;
+
+                }else if(g.grid[shot]=='-'){//if target is empty
+                    play[shot]='X';
+                }
+            }else if(play[shot]=='X'){//if target has been selected before
+                JOptionPane.showMessageDialog(null, "Please select an unrevealed space");
+                i--;
             }
         }
         
         
-    }
+    }//main
     
-}
+
+}//class
